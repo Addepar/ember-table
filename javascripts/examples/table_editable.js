e8917221bfdc1d73b616aea31c90640c2768635d
Foon Wang Pong <peter@addepar.com>
2012-12-16 16:02:02 -0800

    updating editable table example

--- javascripts/examples/table_editable.js
+++ javascripts/examples/table_editable.js
@@ -9,21 +9,53 @@
     isEditing: false,
     type: 'text',
     innerTextField: Ember.TextField.extend({
+      typeBinding: 'parentView.type',
       valueBinding: 'parentView.cellContent',
       didInsertElement: function() {
         return this.$().focus();
       },
-      blur: function() {
+      blur: function(event) {
         return this.set('parentView.isEditing', false);
       }
     }),
     onRowContentDidChange: Ember.observer(function() {
       return this.set('isEditing', false);
     }, 'rowContent'),
-    editLabel: function(event) {
+    click: function(event) {
       this.set('isEditing', true);
       return event.stopPropagation();
     }
+  });
+
+  App.TableEditableExample.DatePickerTableCell = App.TableEditableExample.EditableTableCell.extend({
+    type: 'date'
+  });
+
+  App.TableEditableExample.RatingTableCell = Ember.Table.TableCell.extend({
+    classNames: 'rating-table-cell',
+    templateName: 'rating-table-cell',
+    didInsertElement: function() {
+      this._super();
+      return this.onRowContentDidChange();
+    },
+    applyRating: function(rating) {
+      var span;
+      this.$('.rating span').removeClass('active');
+      span = this.$('.rating span').get(rating);
+      return $(span).addClass('active');
+    },
+    click: function(event) {
+      var rating;
+      rating = this.$('.rating span').index(event.target);
+      if (rating === -1) {
+        return;
+      }
+      this.get('column').setCellContent(this.get('rowContent'), rating);
+      return this.applyRating(rating);
+    },
+    onRowContentDidChange: Ember.observer(function() {
+      return this.applyRating(this.get('cellContent'));
+    }, 'cellContent')
   });
 
   App.TableEditableExample.TableController = Ember.Table.TableController.extend({
@@ -33,29 +65,32 @@
     numRows: 100000,
     rowHeight: 30,
     columns: Ember.computed(function() {
-      var columnNames, columns, dateColumn, entryColumn;
-      columnNames = ['open', 'high', 'low', 'close', 'volume'];
-      entryColumn = Ember.Table.ColumnDefinition.create({
-        columnWidth: 100,
-        headerCellName: 'Entry',
-        tableCellViewClass: 'App.TableEditableExample.EditableTableCell',
-        getCellContent: function(row) {
-          return row['index'];
-        }
-      });
+      var columnNames, columns, dateColumn, ratingColumn;
+      columnNames = ['open', 'close'];
       dateColumn = Ember.Table.ColumnDefinition.create({
         columnWidth: 150,
         headerCellName: 'Date',
-        tableCellViewClass: 'App.TableEditableExample.EditableTableCell',
+        tableCellViewClass: 'App.TableEditableExample.DatePickerTableCell',
         getCellContent: function(row) {
-          return row['date'].toDateString();
+          return row['date'].toString('yyyy-MM-dd');
+        }
+      });
+      ratingColumn = Ember.Table.ColumnDefinition.create({
+        columnWidth: 150,
+        headerCellName: 'Analyst Rating',
+        tableCellViewClass: 'App.TableEditableExample.RatingTableCell',
+        getCellContent: function(row) {
+          return row['rating'];
+        },
+        setCellContent: function(row, value) {
+          return row['rating'] = value;
         }
       });
       columns = columnNames.map(function(key, index) {
         var name;
         name = key.charAt(0).toUpperCase() + key.slice(1);
         return Ember.Table.ColumnDefinition.create({
-          columnWidth: 100,
+          columnWidth: 150,
           headerCellName: name,
           tableCellViewClass: 'App.TableEditableExample.EditableTableCell',
           getCellContent: function(row) {
@@ -63,8 +98,8 @@
           }
         });
       });
+      columns.unshift(ratingColumn);
       columns.unshift(dateColumn);
-      columns.unshift(entryColumn);
       return columns;
     }).property(),
     content: Ember.computed(function() {
@@ -74,17 +109,14 @@
         for (var _i = 0, _ref = this.get('numRows'); 0 <= _ref ? _i < _ref : _i > _ref; 0 <= _ref ? _i++ : _i--){ _results.push(_i); }
         return _results;
       }).apply(this).map(function(num, idx) {
-        var date;
-        date = new Date();
-        date.setDate(date.getDate() + idx);
         return {
           index: idx,
-          date: date,
+          date: Date.today().add({
+            days: idx
+          }),
           open: Math.random() * 100 - 50,
-          high: Math.random() * 100 - 50,
-          low: Math.random() * 100 - 50,
           close: Math.random() * 100 - 50,
-          volume: Math.random() * 1000000
+          rating: Math.round(Math.random() * 4)
         };
       });
     }).property('numRows')
