5adbc5327384718e14e09a8187896afb3a579d75
Foon Wang Pong <peter@addepar.com>
2012-12-23 19:30:57 -0800

    removed group helper as a dependency

--- grunt.js
+++ grunt.js
@@ -1,6 +1,6 @@
 var DIST_FILE     = 'ember-table.js';
 var DIST_FILE_MIN = 'ember-table.min.js';
-var BUILD_TASKS   = 'coffee handlebars concat';
+var BUILD_TASKS   = 'coffee handlebars concat min';
 var JS_FILES      = [
   'build/utils/scrollbar_width_helper.js',
   'build/utils/resize_handler.js',
