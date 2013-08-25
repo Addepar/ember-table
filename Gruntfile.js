b8e2d60537de4448475e094c2e2a08c4067705d3
korczis <korczis@gmail.com>
2013-08-25 07:40:05 +0200

    Basic QUnit integration (use grunt qunit task)

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browserify2');
@@ -122,6 +123,10 @@ module.exports = function (grunt) {
                     ]
                 }
             }
+        },
+
+        qunit: {
+            all: ['tests/*.html']
         },
 
         uglify: {
---
+++ tests/tests.js
@@ -0,0 +1,5 @@
+module("Basic Tests");
+
+test("Controllers", function() {
+    notEqual(null, Ember.Table.TableController, "Ember.Table.TableController exists");
+});
