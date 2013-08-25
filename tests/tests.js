65b4fe6416c7ae605aa213190241121bf727da5a
korczis <korczis@gmail.com>
2013-08-25 08:02:03 +0200

    Added templates presence tests

--- tests/tests.js
+++ tests/tests.js
@@ -12,4 +12,14 @@ test("Views", function() {
     notEqual(null, Ember.Table.HeaderCell, "Ember.Table.HeaderCell exists");
     notEqual(null, Ember.Table.ScrollContainer, "Ember.Table.ScrollContainer exists");
     notEqual(null, Ember.Table.ScrollPanel, "Ember.Table.ScrollPanel exists");
-})
+});
+
+test("Templates", function() {
+    notEqual(null, Ember.TEMPLATES['body-container'], "Ember.TEMPLATES['body-container'] exists");
+    notEqual(null, Ember.TEMPLATES['footer-container'], " Ember.TEMPLATES['footer-container'] exists");
+    notEqual(null, Ember.TEMPLATES['header-cell'], "Ember.TEMPLATES['header-cell'] exists");
+    notEqual(null, Ember.TEMPLATES['header-container'], "Ember.TEMPLATES['header-container'] exists");
+    notEqual(null, Ember.TEMPLATES['header-row'], "Ember.TEMPLATES['header-row'] exists");
+    notEqual(null, Ember.TEMPLATES['table-row'], " Ember.TEMPLATES['table-row'] exists");
+    notEqual(null, Ember.TEMPLATES['tables-container'], "Ember.TEMPLATES['tables-container'] exists");
+});
