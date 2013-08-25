0bb39eac174dd7840ddfaea48260132706938344
korczis <korczis@gmail.com>
2013-08-25 07:56:05 +0200

    Test views presence

--- tests/tests.js
+++ tests/tests.js
@@ -3,3 +3,13 @@ module("Basic Tests");
 test("Controllers", function() {
     notEqual(null, Ember.Table.TableController, "Ember.Table.TableController exists");
 });
+
+test("Views", function() {
+    notEqual(null, Ember.Table.TablesContainer, "Ember.Table.TablesContainer exists");
+    notEqual(null, Ember.Table.TableContainer, "Ember.Table.TableContainer exists");
+    notEqual(null, Ember.Table.TableCell, "Ember.Table.TableCell exists");
+    notEqual(null, Ember.Table.HeaderRow, "Ember.Table.HeaderRow exists");
+    notEqual(null, Ember.Table.HeaderCell, "Ember.Table.HeaderCell exists");
+    notEqual(null, Ember.Table.ScrollContainer, "Ember.Table.ScrollContainer exists");
+    notEqual(null, Ember.Table.ScrollPanel, "Ember.Table.ScrollPanel exists");
+})
