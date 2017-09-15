2838570d850221b7625f81f2c02fc574bcb74aa8
Billy Pham <billy.pham@addepar.com>
2017-09-15 14:43:56 -0700

    Update babel

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    babel: {
+    'ember-cli-babel': {
       includePolyfill: true
     },
   });
