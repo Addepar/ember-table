0f0e7df735f46c0ed660aefc3835b617065f5668
Billy Pham <billy.pham@addepar.com>
2017-11-14 14:32:05 -0800

    Fix lint

--- tests/dummy/app/routes/custom-header.js
+++ tests/dummy/app/routes/custom-header.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-import { action } from 'ember-decorators/object';
 import { property } from '../utils/class';
 
 export default class CustomHeader extends Route {
