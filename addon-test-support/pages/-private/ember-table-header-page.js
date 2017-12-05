a9622b9d9ac9f90d3177abbcebecef22a48cee98
Billy Pham <billy.pham@addepar.com>
2017-12-05 12:01:20 -0800

    Table PageObject  (#458)

---
+++ addon-test-support/pages/-private/ember-table-header-page.js
@@ -0,0 +1,40 @@
+import { collection, count } from 'ember-classy-page-object';
+import { findElement } from 'ember-classy-page-object/extend';
+import { getter } from 'ember-cli-page-object/macros';
+
+export default {
+  scope: 'thead',
+
+  /**
+   * List of columns in the header.
+   */
+  columns: collection({
+    scope: 'tr:eq(0) th',
+
+    /**
+     * Retrieves selected header cell width.
+     */
+    width: getter(function() {
+      return findElement(this).offsetWidth;
+    }),
+
+    /**
+     * Retrieves selected header cell height.
+     */
+    height: getter(function() {
+      return findElement(this).offsetHeight;
+    }),
+  }),
+
+  /**
+   * Returns the number of columns in header.
+   */
+  columnCount: count('tr:eq(0) th'),
+
+  /**
+   * Returns header width.
+   */
+  width: getter(function() {
+    return findElement(this).offsetWidth;
+  })
+};
