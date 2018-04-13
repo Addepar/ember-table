903afed020d38cc5eef136fd59c2bc13c107a9e7
Christopher Garrett <opensource@pzuraq.com>
2018-04-12 17:06:53 -0700

    [refactor] Combine ember-table and tree-table
    Based on the recent work on CollapseTree we now have a cheap data
    structure which can easily wrap a top level array. This means we can
    combine {{tree-table}} with {{ember-table}}, with the former "plain
    array" functionality of {{ember-table}} handled as the case where we
    have a tree with no children on any nodes.
    
    There are more optimizations that'll go into this work - in particular
    I think we can clean up the selection flows a bit for tree tables.
    For now, this is consolidation pre-major refactor.

--- addon/components/tree-table-body.js
+++
@@ -1,6 +0,0 @@
-import EmberTableBody from './ember-table-body';
-import layout from '../templates/components/tree-table-body';
-
-export default class TreeTableBody extends EmberTableBody {
-  layout = layout;
-}
