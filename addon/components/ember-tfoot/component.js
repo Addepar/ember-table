13570b6b9fd3a790f938785a1b8bde649212ff26
Andy Lei <andy@addepar.com>
2018-05-24 22:21:48 -0400

    test to repro footer bug (#535)
    This test fails with:
    
    ```
     ember try:one ember-1.11
    ```
    
    Haven't been able to fix this bug yet.

--- addon/components/ember-tfoot/component.js
+++ addon/components/ember-tfoot/component.js
@@ -1,5 +1,7 @@
 import EmberTBody from '../ember-tbody/component';
+import { A as emberA } from '@ember/array';
 
+import { computed } from '@ember-decorators/object';
 import { tagName } from '@ember-decorators/component';
 
 import layout from './template';
@@ -7,4 +9,18 @@ import layout from './template';
 @tagName('tfoot')
 export default class EmberTFoot extends EmberTBody {
   layout = layout;
+
+  @computed('wrappedRows.[]')
+  get wrappedRowArray() {
+    let wrappedRows = this.get('wrappedRows');
+    let wrappedRowsLength = wrappedRows.get('length');
+
+    let arr = [];
+
+    for (let i = 0; i < wrappedRowsLength; i++) {
+      arr.push(wrappedRows.objectAt(i));
+    }
+
+    return emberA(arr);
+  }
 }
