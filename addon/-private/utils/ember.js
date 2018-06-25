7e2e4785c20c34e8aeeea9e8802901374e3ada56
Rich Glazerman <rglazerman@squareup.com>
2018-06-25 13:31:35 -0400

    Update notifyPropertyChange polyfill to work with proper versions
    notifyPropertyChange exists in Ember 3.1 globals, but not Ember 3.0. Update the polyfill to look for versions of
    Ember gte 3.1.0, not 3.0.0

--- addon/-private/utils/ember.js
+++ addon/-private/utils/ember.js
@@ -1,6 +1,6 @@
 /* globals Ember */
 import { gte } from 'ember-compatibility-helpers';
 
-export const notifyPropertyChange = gte('3.0.0')
+export const notifyPropertyChange = gte('3.1.0')
   ? Ember.notifyPropertyChange
   : Ember.propertyDidChange;
