65b00ad8c83630a88c9f0950ec1b271fad9987ef
Cy Klassen <cytklassen@gmail.com>
2018-12-24 15:49:46 -0800

    [bugfix] Disable ember/new-module-imports rule for special case

--- addon/-private/utils/ember.js
+++ addon/-private/utils/ember.js
@@ -1,6 +1,7 @@
 /* globals Ember */
 import { gte } from 'ember-compatibility-helpers';
 
+/* eslint-disable ember/new-module-imports */
 export const notifyPropertyChange = gte('3.1.0')
   ? Ember.notifyPropertyChange
   : Ember.propertyDidChange;
