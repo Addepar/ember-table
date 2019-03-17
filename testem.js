b477a98da0231e7d5fa386bf7d7c1769b4fb66cf
Jeldrik Hanschke <admin@jhanschke.de>
2019-03-17 10:04:02 +0100

    CI: run tests against Firefox

--- testem.js
+++ testem.js
@@ -1,7 +1,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
+  launch_in_ci: process.env.TESTEM_LAUNCH ? process.env.TESTEM_LAUNCH.split(',') : ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: {
@@ -16,5 +16,12 @@ module.exports = {
         '--window-size=1440,900',
       ].filter(Boolean),
     },
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '--headless',
+        '--window-size=1440,900',
+      ],
+    }
   },
 };
