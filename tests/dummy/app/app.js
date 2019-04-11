573752907e64d0414696dfbf2ffbdf3e630f35f6
Chris Garrett <cgarrett@linkedin.com>
2019-04-11 13:32:18 -0700

    fix release tests

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,16 +1,14 @@
-/* globals define */
+/* globals define, require */
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 import EmberRouter from '@ember/routing/router';
 
-import { lte } from 'ember-compatibility-helpers';
-
 // Including ember-cli-addon-docs breaks certain versions of Ember when testing
 // but they also break if we remove it. This defines a stub router which should
 // prevent breakage.
-if (lte('2.5.0')) {
+if (!require.entries['ember-cli-addon-docs/router']) {
   define('ember-cli-addon-docs/router', () => {
     return EmberRouter;
   });
