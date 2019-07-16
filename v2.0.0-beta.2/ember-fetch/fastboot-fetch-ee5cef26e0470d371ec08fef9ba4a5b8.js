75bf889a52baac8ef50c9f874ca659dc1a89c4e5
Cory Forsyth <cory.forsyth@addepar.com>
2019-07-16 14:31:58 -0400

    Deployed 15cf3ae

--- v2.0.0-beta.2/ember-fetch/fastboot-fetch-ee5cef26e0470d371ec08fef9ba4a5b8.js
+++
@@ -1,2 +0,0 @@
-define("fetch",["exports"],function(e){var t=FastBoot.require("abortcontroller-polyfill/dist/cjs-ponyfill"),o=FastBoot.require("node-fetch"),r=t.abortableFetch({fetch:o,Request:o.Request})
-e.default=r.fetch,e.Request=r.Request,e.Headers=o.Headers,e.Response=o.Response,e.AbortController=t.AbortController}),define("fetch/ajax",["exports"],function(){throw new Error("You included `fetch/ajax` but it was renamed to `ember-fetch/ajax`")})
