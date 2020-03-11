638a593f3264e696adc514b903f580c7db3a8ff6
Kevin Pfefferle <kevin.pfefferle@addepar.com>
2020-03-11 15:31:06 -0400

    Toggle scroll indicators based on scrolling

--- addon/components/-private/scroll-indicators/component.js
+++ addon/components/-private/scroll-indicators/component.js
@@ -1,10 +1,10 @@
 import Component from '@ember/component';
 import { readOnly } from '@ember/object/computed';
+import { addObserver } from 'ember-table/-private/utils/observer';
 import layout from './template';
 
 export default Component.extend({
   layout,
-
   tagName: '',
 
   /**
@@ -16,5 +16,58 @@ export default Component.extend({
   */
   api: null,
 
+  scrollElement: null,
+  showLeft: true,
+  showRight: false,
+
+  columnTree: readOnly('api.columnTree'),
   enableScrollIndicators: readOnly('api.enableScrollIndicators'),
+  tableScrollId: readOnly('api.tableId'),
+
+  _addScrollListener() {
+    this._boundOnScroll = this._onScroll.bind(this);
+    this.get('scrollElement').addEventListener('scroll', this._boundOnScroll);
+  },
+
+  _removeScrollListener() {
+    this.get('scrollElement').removeEventListener('scroll', this._boundOnScroll);
+  },
+
+  _onScroll(e) {
+    this._updateIndicatorShow(e.target);
+  },
+
+  _updateIndicatorShow(scrollElement) {
+    let scrollRect = scrollElement.getBoundingClientRect();
+    let tableRect = scrollElement.querySelector('table').getBoundingClientRect();
+    let xDiff = scrollRect.x - tableRect.x;
+    let widthDiff = tableRect.width - scrollRect.width;
+    this.set('showLeft', xDiff !== 0);
+    this.set('showRight', xDiff !== widthDiff);
+  },
+
+  _updateScrollListener() {
+    if (this.get('enableScrollIndicators')) {
+      this._addScrollListener();
+    } else {
+      this._removeScrollListener();
+    }
+  },
+
+  init() {
+    this._super(...arguments);
+    let scrollElement = document.getElementById(this.get('tableScrollId'));
+    this.set('scrollElement', scrollElement);
+    this._updateIndicatorShow(scrollElement);
+    if (this.get('enableScrollIndicators')) {
+      this._addScrollListener();
+    }
+    addObserver(this, 'enableScrollIndicators', this._updateScrollListener);
+  },
+
+  willDestroy() {
+    if (this.get('enableScrollIndicators')) {
+      this._removeScrollListener();
+    }
+  },
 });
