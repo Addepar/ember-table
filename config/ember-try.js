8b6fce2729851a9278aa21a7ef40bd38727001c4
Cory Forsyth <cory.forsyth@addepar.com>
2020-11-17 14:14:17 -0500

    do not test on ember 2.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -10,35 +10,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-2.0',
-          bower: {
-            dependencies: {
-              ember: '~2.0',
-              'ember-cli-shims': '0.1.0',
-              'ember-data': '~2.1',
-            },
-            resolutions: {
-              ember: '~2.0',
-              'ember-cli-shims': '0.1.0',
-              'ember-data': '~2.1',
-            },
-          },
-          npm: {
-            resolutions: {
-              // ember-math-helpers has a dependency on newer htmlbars and it
-              // conflicts this older version of ember with a message:
-              // "ember-cli-htmlbars: Cannot find the ember-source addon"...
-              'ember-cli-htmlbars': '^3',
-            },
-            devDependencies: {
-              'ember-cli-shims': null,
-              'ember-source': null,
-              'ember-cli-addon-docs': null,
-              'ember-angle-bracket-invocation-polyfill': null,
-            },
-          },
-        },
-        {
           name: 'ember-lts-2.4',
           bower: {
             dependencies: {
