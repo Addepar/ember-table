50cc1331aec8c238a56c19e090888dd5458d320f
Alex Navasardyan <alex.navasardyan@gmail.com>
2021-02-05 08:43:01 -0500

    Remove Ember 2.4 from the test matrix
    Based on the feedback on
    https://github.com/Addepar/ember-table/issues/819#issuecomment-772814116,
    we are dropping Ember 2.4 testing matrix.

--- config/ember-try.js
+++ config/ember-try.js
@@ -10,37 +10,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.4',
-          bower: {
-            dependencies: {
-              ember: 'components/ember#lts-2-4',
-            },
-            resolutions: {
-              ember: 'lts-2-4',
-            },
-          },
-          npm: {
-            resolutions: {
-              // ember-math-helpers has a dependency on newer htmlbars and it
-              // conflicts this older version of ember with a message:
-              // "ember-cli-htmlbars: Cannot find the ember-source addon"...
-              'ember-cli-htmlbars': '^3',
-              // The 1.2.2 release causes an error related to a missing 'match'
-              // property which is due to the lack of ember-source.
-              // See https://github.com/pzuraq/ember-compatibility-helpers/issues/47
-              'ember-compatibility-helpers': '1.2.1',
-            },
-            dependencies: {
-              'ember-compatibility-helpers': '1.2.1',
-            },
-            devDependencies: {
-              'ember-source': null,
-              'ember-cli-addon-docs': null,
-              'ember-angle-bracket-invocation-polyfill': null,
-            },
-          },
-        },
-        {
           name: 'ember-lts-2.8',
           bower: {
             dependencies: {
