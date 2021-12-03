3a934ae2d11b2af59624f5115e0d8f990c77e6f3
Kevin Pfefferle <kevin.pfefferle@addepar.com>
2021-12-03 10:17:17 -0500

    Drop 2.8; re-enable release scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,36 +11,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.8',
-          bower: {
-            dependencies: {
-              ember: 'components/ember#lts-2-8',
-            },
-            resolutions: {
-              ember: 'lts-2-8',
-            },
-          },
-          npm: {
-            resolutions: {
-              // ember-math-helpers has a dependency on newer htmlbars and it
-              // conflicts this older version of ember with a message:
-              // "ember-cli-htmlbars: Cannot find the ember-source addon"...
-              'ember-cli-htmlbars': '^3',
-              // The 1.2.2 release causes an error related to a missing 'match'
-              // property which is due to the lack of ember-source.
-              // See https://github.com/pzuraq/ember-compatibility-helpers/issues/47
-              'ember-compatibility-helpers': '1.2.1',
-            },
-            dependencies: {
-              'ember-compatibility-helpers': '1.2.1',
-            },
-            devDependencies: {
-              'ember-source': null,
-              'ember-angle-bracket-invocation-polyfill': null,
-            },
-          },
-        },
-        {
           name: 'ember-lts-2.12',
           npm: {
             devDependencies: {
