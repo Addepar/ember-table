c46f398d03ec0cccbe6e8831d0642a0fea1921e7
Matthew Beale <matt.beale@madhatted.com>
2022-01-14 20:37:22 -0500

    Drop compat checks which are no longer needed

--- tests/dummy/app/pods/application/controller.js
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-import { gte } from 'ember-compatibility-helpers';
-import { computed } from '@ember/object';
-
-export default Controller.extend({
-  canShowAddonDocs: computed(function() {
-    return gte('2.8.0');
-  }),
-});
