f0dcad37e384dd3e63814d6d267aadb08533ec87
Alex Navasardyan <alex.navasardyan@gmail.com>
2022-08-30 08:44:59 -0400

    Register raf waiter as a part of `setupForTest`
    Addresses #956

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -1,10 +1,11 @@
 import TablePage from './pages/ember-table';
-
+import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import { setSetupRowCountForTest } from 'ember-table/components/ember-tbody/component';
 import { setupTHeadForTest } from 'ember-table/components/ember-thead/component';
 import { setSimpleCheckboxForTest } from 'ember-table/components/ember-td/component';
 
 function setupForTest() {
+  registerWaiter();
   setSetupRowCountForTest(true);
   setupTHeadForTest(true);
   setSimpleCheckboxForTest(true);
