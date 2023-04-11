21f1980b6e1e329e31a7939875fcaec255ae84d9
Matthew Beale <matt.beale@addepar.com>
2023-04-11 11:21:49 -0400

    Add rootURL for production docs output

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -43,5 +43,10 @@ module.exports = function(environment) {
     ENV.APP.autoboot = false;
   }
 
+  if (environment === 'production') {
+    // Allow ember-cli-addon-docs to update the rootURL in compiled assets
+    ENV.rootURL = 'ADDON_DOCS_ROOT_URL';
+  }
+
   return ENV;
 };
