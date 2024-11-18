5b2b9e0cd079f5053299dd678abc91ee781a2d3b
Jordy Tack <jordy.tack@otainsight.com>
2024-11-18 19:19:15 +0100

    Revert "Added expandColumnsToMax to ensure maxWidth is used"
    This reverts commit b1be62cc12b09733ebf9137dff1979e17aff2f2d.

--- addon/-private/column-tree.js
+++ addon/-private/column-tree.js
@@ -763,7 +763,6 @@ export default EmberObject.extend({
       return;
     }
 
-    let widthConstraint = get(this, 'widthConstraint');
     let isSlackModeEnabled = get(this, 'isSlackModeEnabled');
 
     // For gte-container-slack, first try to expand columns to their maxWidth
