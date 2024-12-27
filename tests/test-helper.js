c23927bec06c3de88b11322a8a85b9e0b8e11a9d
Cory Forsyth <cory.forsyth@addepar.com>
2024-12-27 13:04:12 +0100

    Due to ember-qunit upgrade, explicitly load tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,13 +2,17 @@ import Application from '../app';
 import config from '../config/environment';
 import registerRAFWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 import QUnit from 'qunit';
 import {
   setup as setupWarnHandlers,
   teardown as teardownWarnHandlers,
 } from './helpers/warn-handlers';
 import { setupForTest as setupEmberTableForTest } from 'ember-table/test-support';
+import { loadTests } from 'ember-qunit/test-loader';
+
+setupEmberOnerrorValidation();
+loadTests();
 
 registerRAFWaiter();
 setApplication(Application.create(config.APP));
