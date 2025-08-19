f729f26a913f91f66ff22a3f16b06718176b1c8f
Kevin Pfefferle <kevin.pfefferle@addepar.com>
2025-08-19 17:52:34 -0400

    Mark rows as even or odd for styling purposes (#1190)

--- tests/integration/components/row-test.js
+++ tests/integration/components/row-test.js
@@ -14,6 +14,8 @@ let table = new TablePage({
   body: {
     rows: collection({
       isCustomRow: hasClass('custom-row'),
+      isEven: hasClass('is-even'),
+      isOdd: hasClass('is-odd'),
     }),
   },
 });
@@ -34,6 +36,17 @@ const USE_EMBER_ARRAY_PARAMETERS = {
 
 module('Integration | row', function() {
   parameterizedComponentModule('basic', USE_EMBER_ARRAY_PARAMETERS, function() {
+    test('marks rows as even or odd', async function(assert) {
+      await generateTable(this);
+
+      assert.true(table.rows.objectAt(0).isEven, 'First row is even');
+      assert.true(table.rows.objectAt(1).isOdd, 'Second row is odd');
+      assert.true(table.rows.objectAt(2).isEven, 'Third row is even');
+      assert.true(table.rows.objectAt(3).isOdd, 'Fourth row is odd');
+      assert.true(table.rows.objectAt(4).isEven, 'Fifth row is even');
+      assert.true(table.rows.objectAt(5).isOdd, 'Sixth row is odd');
+    });
+
     test('can use a custom row component', async function(assert) {
       await generateTable(this, {
         rowComponent: 'custom-row',
